f17a9b6aff0943ebb657a6710064533fdce21894
Gavin Hill <gavin@highpeak.co.za>
2021-05-15 13:25:30 +0000

    key bindings

--- zsh/p10k.zsh
+++ zsh/p10k.zsh
@@ -31,7 +31,8 @@
 
   # The list of segments shown on the left. Fill it with the most important segments.
   typeset -g POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(
-    os_icon                 # os identifier
+    # os_icon                 # os identifier
+    user
     dir                     # current directory
     vcs                     # git status
     # prompt_char           # prompt symbol
