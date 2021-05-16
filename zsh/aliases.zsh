cbd87534bce88f3648ab5f25107579513c91a1dc
Gavin Hill <gavin@highpeak.co.za>
2021-05-16 14:07:39 +0000

    Beter aliases and work on tmux

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,2 +1,5 @@
 ## Colorize the ls output ##
 alias ls='ls --color=auto'
+alias vimalias='vim ~/.dotfiles/zsh/aliases.zsh;source ~/.dotfiles/zsh/aliases.zsh'
+alias logsys='multitail /var/log/syslog'
+
