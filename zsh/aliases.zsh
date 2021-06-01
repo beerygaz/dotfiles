b56983c5afb3ad1d71053bcf8eb69384b205b34a
Gavin Hill <gavin@highpeak.co.za>
2021-06-01 20:17:11 +0200

    Fixed macos ls alias

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,5 +1,5 @@
 ## Colorize the ls output ##
-alias ls='ls --color=auto'
+alias ls='ls -FHG'
 alias vimalias='vim ~/.dotfiles/zsh/aliases.zsh;source ~/.dotfiles/zsh/aliases.zsh'
 alias logsys='multitail /var/log/syslog'
 
