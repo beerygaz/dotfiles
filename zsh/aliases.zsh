cead15d614f08a2e4e3768491679de6d11c789ed
Gavin Hill <gavin@highpeak.co.za>
2023-07-31 08:14:46 +0200

    Darwin aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -2,5 +2,11 @@
 alias ls='ls -FHG'
 alias vimalias='vim ~/.dotfiles/zsh/aliases.zsh;source ~/.dotfiles/zsh/aliases.zsh'
 alias logsys='multitail /var/log/syslog'
-alias cat=batcat
+# macOS aliasses
+if [[ $OSTYPE == darwin* ]]; then
+  alias flush='dscacheutil -flushcache'
+  alias cat=bat
+else
+  alias cat=batcat
+fi
 
