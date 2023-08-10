d4a19140fd10a0e86e4d928509bc8419edbc41e4
Gavin Hill <gavin@highpeak.co.za>
2023-08-10 13:10:05 +0200

    smoe new aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -2,6 +2,7 @@
 alias ls='ls -FHG'
 alias vimalias='vim ~/.dotfiles/zsh/aliases.zsh;source ~/.dotfiles/zsh/aliases.zsh'
 alias logsys='multitail /var/log/syslog'
+alias 'az al'='az account list -o table'
 # macOS aliasses
 if [[ $OSTYPE == darwin* ]]; then
   alias flush='dscacheutil -flushcache'
